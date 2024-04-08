42888c84f03b1716ea6e24c0cc8b5ad6c98a42b7
RobertGarabetian <rgarabe101@student.glendale.edu>
2024-04-08 00:27:09 -0700

    couple of changes

--- scripts.js
+++ scripts.js
@@ -22,20 +22,72 @@
  *    with the string you added to the array, but a broken image.
  * 
  */
+// import movies from './data.js'
 
-
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
-];
 // Your final submission should have much more data than this, and 
 // you should use more than just an array of strings to store it all.
+const movies = [
+    {
+    "title": "Arrival",
+    "poster": "https://media.posterlounge.com/img/products/660000/651583/651583_poster.jpg",
+    "rating": 94,
+    "releaseDate": new Date(2016,9,2)
+    },
+    {
+    "title": "2001: A Space Odyssey",
+    "poster": "https://i.ebayimg.com/images/g/gbkAAOSwHP5ebNKR/s-l1600.jpg",
+    "rating": 92,
+    "releaseDate": new Date(1968,4,2)
+    },
+    {
+    "title": "Megamind",
+    "poster": "https://digitalmoviedeals.nbcuniversal.com/cdn/shop/files/0fa342e3-605a-4b5d-b4c3-c52d4d30efcb_808fda86-882d-476e-941d-c8ae245dfcae.jpg?v=1710170912",
+    "rating": 73,
+    "releaseDate": new Date(2010,10,30)
+    },
+    {
+    "title": "Blade Runner 2049",
+    "poster": "https://m.media-amazon.com/images/I/61WfeCSu6JL._AC_UF894,1000_QL80_.jpg",
+    "rating": 88,
+    "releaseDate": new Date(2017,10,6)
+    },
+    {
+    "title": "Interstellar",
+    "poster": "https://m.media-amazon.com/images/I/71dN1QYnf+L._AC_UF894,1000_QL80_.jpg",
+    "rating": 73,
+    "releaseDate": new Date(2014,10,26)
+    },
+    {
+    "title": "Alien",
+    "poster": "https://i.etsystatic.com/23402008/r/il/d58671/2996343001/il_570xN.2996343001_aomg.jpg",
+    "rating": 93,
+    "releaseDate": new Date(1979,5,25)
+    },
+    {
+    "title": "Dune: Part One",
+    "poster": "https://m.media-amazon.com/images/I/61QbqeCVm0L.jpg",
+    "rating": 83,
+    "releaseDate": new Date(2021,10,22)
+    },
+    {
+    "title": "Dune: Part Two",
+    "poster": "https://m.media-amazon.com/images/I/51ZM+aTI7rL._AC_UF894,1000_QL80_.jpg",
+    "rating": 93,
+    "releaseDate": new Date(2024,3,1)
+    },
+    {
+    "title": "Ender's Game",
+    "poster": "https://m.media-amazon.com/images/I/61KLKWEUEgL._AC_UF894,1000_QL80_.jpg",
+    "rating": 62,
+    "releaseDate": new Date(2013,11,1)
+    },
+    {
+    "title": "The Matrix",
+    "poster": "https://i.ebayimg.com/images/g/zwAAAOSwCGVX5wN4/s-l1200.jpg",
+    "rating": 83,
+    "releaseDate": new Date(1999,3,31)
+    }
+];
 
 
 // This function adds cards the page to display the data in the array
@@ -43,28 +95,42 @@ function showCards() {
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerHTML = "";
     const templateCard = document.querySelector(".card");
-    
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
 
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
-
+    movies.map((movie)=>{
         const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
+        editCardContent(nextCard, movie.title, movie.poster, movie.rating, movie.releaseDate); // Edit title and image
         cardContainer.appendChild(nextCard); // Add new card to the container
-    }
+    })
+
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+function showMovie(movie){
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+    const templateCard = document.querySelector(".card");
+
+    const nextCard = templateCard.cloneNode(true); // Copy the template card
+    editCardContent(nextCard, movie.title, movie.poster, movie.rating, movie.releaseDate); // Edit title and image
+    cardContainer.appendChild(nextCard); // Add new card to the container
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    document.getElementById("searchForm").addEventListener("submit", function(event){
+        event.preventDefault();
+    
+        const searchInput = document.getElementById("searchInput");
+        const searchQuery = searchInput.value;
+        
+        movies.map((movie)=>{
+            if (searchQuery == movie.title){
+                showMovie(movie);
+            }
+        }) 
+    });
+});
+
+
+function editCardContent(card, newTitle, newImageURL, rating, date) {
     card.style.display = "block";
 
     const cardHeader = card.querySelector("h2");
@@ -74,14 +140,20 @@ function editCardContent(card, newTitle, newImageURL) {
     cardImage.src = newImageURL;
     cardImage.alt = newTitle + " Poster";
 
-    // You can use console.log to help you debug!
-    // View the output by right clicking on your website,
-    // select "Inspect", then click on the "Console" tab
+    const cardRating = card.querySelector(".rating");
+    cardRating.textContent = (rating + "% Rotten Tomatoes");
+
+    const cardDate = card.querySelector(".date");
+    cardDate.textContent = formatDate(date);
+
     console.log("new card:", newTitle, "- html: ", card);
 }
 
 // This calls the addCards() function when the page is first loaded
 document.addEventListener("DOMContentLoaded", showCards);
+
+
+
 
 function quoteAlert() {
     console.log("Button Clicked!")
@@ -89,6 +161,34 @@ function quoteAlert() {
 }
 
 function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
+    movies.pop(); // Remove last item in titles array
     showCards(); // Call showCards again to refresh
+}
+
+function sortByName(){
+    movies.sort((a, b) => {
+        if (a.title < b.title) {
+            return -1;
+        } else if (a.title > b.title) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+    showCards();
+}
+function sortByDate(){
+    movies.sort((a, b) => b.releaseDate - a.releaseDate);
+    showCards();
+}
+function sortByRating(){
+    movies.sort((a, b) => b.rating - a.rating);
+    showCards();
+}
+
+
+
+function formatDate(date) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(date).toLocaleDateString('en-US', options);
 }
