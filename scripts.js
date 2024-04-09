16e8efe608c088620b8a0bf8768d8571399e180e
RobertGarabetian <rgarabe101@student.glendale.edu>
2024-04-09 13:42:48 -0700

    Almost done

--- scripts.js
+++ scripts.js
@@ -26,69 +26,86 @@
 
 // Your final submission should have much more data than this, and 
 // you should use more than just an array of strings to store it all.
-const movies = [
+let movies = [
     {
     "title": "Arrival",
     "poster": "https://media.posterlounge.com/img/products/660000/651583/651583_poster.jpg",
     "rating": 94,
-    "releaseDate": new Date(2016,9,2)
+    "releaseDate": new Date(2016,9,2),
+    "director": "Denis Villeneuve"
     },
     {
     "title": "2001: A Space Odyssey",
     "poster": "https://i.ebayimg.com/images/g/gbkAAOSwHP5ebNKR/s-l1600.jpg",
     "rating": 92,
-    "releaseDate": new Date(1968,4,2)
+    "releaseDate": new Date(1968,4,2),
+    "director": "Stanley Kubrick"
     },
     {
     "title": "Megamind",
     "poster": "https://digitalmoviedeals.nbcuniversal.com/cdn/shop/files/0fa342e3-605a-4b5d-b4c3-c52d4d30efcb_808fda86-882d-476e-941d-c8ae245dfcae.jpg?v=1710170912",
     "rating": 73,
-    "releaseDate": new Date(2010,10,30)
+    "releaseDate": new Date(2010,10,30),
+    "director": "Tom McGrath"
     },
     {
     "title": "Blade Runner 2049",
     "poster": "https://m.media-amazon.com/images/I/61WfeCSu6JL._AC_UF894,1000_QL80_.jpg",
     "rating": 88,
-    "releaseDate": new Date(2017,10,6)
+    "releaseDate": new Date(2017,10,6),
+    "director": "Denis Villeneuve"
     },
     {
     "title": "Interstellar",
     "poster": "https://m.media-amazon.com/images/I/71dN1QYnf+L._AC_UF894,1000_QL80_.jpg",
     "rating": 73,
-    "releaseDate": new Date(2014,10,26)
+    "releaseDate": new Date(2014,10,26),
+    "director": "Christopher Nolan"
     },
     {
     "title": "Alien",
     "poster": "https://i.etsystatic.com/23402008/r/il/d58671/2996343001/il_570xN.2996343001_aomg.jpg",
     "rating": 93,
-    "releaseDate": new Date(1979,5,25)
+    "releaseDate": new Date(1979,5,25),
+    "director":"Ridley Scott"
     },
     {
     "title": "Dune: Part One",
     "poster": "https://m.media-amazon.com/images/I/61QbqeCVm0L.jpg",
     "rating": 83,
-    "releaseDate": new Date(2021,10,22)
+    "releaseDate": new Date(2021,10,22),
+    "director": "Denis Villeneuve"
     },
     {
     "title": "Dune: Part Two",
     "poster": "https://m.media-amazon.com/images/I/51ZM+aTI7rL._AC_UF894,1000_QL80_.jpg",
     "rating": 93,
-    "releaseDate": new Date(2024,3,1)
+    "releaseDate": new Date(2024,3,1),
+    "director": "Denis Villeneuve"
     },
     {
     "title": "Ender's Game",
     "poster": "https://m.media-amazon.com/images/I/61KLKWEUEgL._AC_UF894,1000_QL80_.jpg",
     "rating": 62,
-    "releaseDate": new Date(2013,11,1)
+    "releaseDate": new Date(2013,11,1),
+    "director": "Gavin Hood"
     },
     {
     "title": "The Matrix",
     "poster": "https://i.ebayimg.com/images/g/zwAAAOSwCGVX5wN4/s-l1200.jpg",
     "rating": 83,
-    "releaseDate": new Date(1999,3,31)
+    "releaseDate": new Date(1999,3,31),
+    "director": "Lana Wachowski & Lily Wachowski"
     }
 ];
 
+const facts = [
+    "The Black Hole In 'Interstellar' Is So Accurate, It Spawned Numerous Academic Papers.",
+    "The Matrix' code represents Japanese sushi recipes.",
+    "Megamind has the same head to body proportion as a toddler, or a Lego minifigure.",
+    "Stanley Kubrick won his only Oscar for 2001: A Space Odyssey.",
+    "For Dune: Part Two, Stellan Skarsg\u00E5rd body makeup took 8 hours to apply and 2 hours to remove for every day of shooting."
+];
 
 // This function adds cards the page to display the data in the array
 function showCards() {
@@ -98,39 +115,12 @@ function showCards() {
 
     movies.map((movie)=>{
         const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, movie.title, movie.poster, movie.rating, movie.releaseDate); // Edit title and image
+        editCardContent(nextCard, movie.title, movie.poster, movie.rating, movie.releaseDate, movie.director); // Edit title and image
         cardContainer.appendChild(nextCard); // Add new card to the container
     })
-
 }
 
-function showMovie(movie){
-    const cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = "";
-    const templateCard = document.querySelector(".card");
-
-    const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, movie.title, movie.poster, movie.rating, movie.releaseDate); // Edit title and image
-    cardContainer.appendChild(nextCard); // Add new card to the container
-}
-
-document.addEventListener("DOMContentLoaded", ()=>{
-    document.getElementById("searchForm").addEventListener("submit", function(event){
-        event.preventDefault();
-    
-        const searchInput = document.getElementById("searchInput");
-        const searchQuery = searchInput.value;
-        
-        movies.map((movie)=>{
-            if (searchQuery == movie.title){
-                showMovie(movie);
-            }
-        }) 
-    });
-});
-
-
-function editCardContent(card, newTitle, newImageURL, rating, date) {
+function editCardContent(card, newTitle, newImageURL, rating, date, director) {
     card.style.display = "block";
 
     const cardHeader = card.querySelector("h2");
@@ -141,7 +131,10 @@ function editCardContent(card, newTitle, newImageURL, rating, date) {
     cardImage.alt = newTitle + " Poster";
 
     const cardRating = card.querySelector(".rating");
-    cardRating.textContent = (rating + "% Rotten Tomatoes");
+    cardRating.textContent = (rating + "%");
+
+    const cardDirector = card.querySelector(".director");
+    cardDirector.textContent = director
 
     const cardDate = card.querySelector(".date");
     cardDate.textContent = formatDate(date);
@@ -149,46 +142,93 @@ function editCardContent(card, newTitle, newImageURL, rating, date) {
     console.log("new card:", newTitle, "- html: ", card);
 }
 
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
 
-
-
-
-function quoteAlert() {
-    console.log("Button Clicked!")
-    alert("I guess I can kiss heaven goodbye, because it got to be a sin to look this good!");
+function factAlert() {
+    let randomNumber = Math.floor(Math.random() * 5)
+    alert(facts[randomNumber])
 }
 
 function removeLastCard() {
-    movies.pop(); // Remove last item in titles array
-    showCards(); // Call showCards again to refresh
+    movies.pop(); 
+    showCards(); 
 }
 
 function sortByName(){
     movies.sort((a, b) => {
         if (a.title < b.title) {
             return -1;
-        } else if (a.title > b.title) {
+        } 
+        else if (a.title > b.title) {
             return 1;
-        } else {
+        } 
+        else {
             return 0;
         }
     });
     showCards();
 }
+
 function sortByDate(){
     movies.sort((a, b) => b.releaseDate - a.releaseDate);
     showCards();
 }
+
 function sortByRating(){
     movies.sort((a, b) => b.rating - a.rating);
     showCards();
 }
 
-
-
 function formatDate(date) {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     return new Date(date).toLocaleDateString('en-US', options);
 }
+
+function openForm() {
+    document.getElementById("contentAdd").style.display = "block";
+}
+function closeForm(){
+    document.getElementById("searchFormAdd").reset();
+    document.getElementById("contentAdd").style.display = "none";
+}
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    showCards();
+
+    document.getElementById("searchFormAdd").addEventListener("submit", function(e){
+        e.preventDefault();
+
+        const inputTitle = document.getElementById("inputTitle");
+        const inputRating = document.getElementById("inputRating");
+        const inputIMG= document.getElementById("inputIMG");
+        const inputDirector = document.getElementById("inputDirector");
+        const inputDate = document.getElementById("inputDate");
+
+ 
+        movies.push({
+            "title": inputTitle.value,
+            "poster": inputIMG.value,
+            "rating": inputRating.value,
+            "releaseDate": new Date(inputDate.value),
+            "director": inputDirector.value
+        })
+        showCards();
+        closeForm();
+    });
+
+
+    document.getElementById("searchForm").addEventListener("submit", function(event){
+        event.preventDefault();
+    
+        const searchInput = document.getElementById("searchInput");
+        const searchQuery = searchInput.value;
+        
+        movies = movies.filter((movie)=>{
+            if (searchQuery == movie.title){
+                return movie
+            }
+        })
+        showCards();
+    });
+});
